70769e8913a686b1b3497b0e586c0c72a1e19063
Shivam609 <61914966+Shivam609@users.noreply.github.com>
2020-03-31 03:39:56 +0100

    first website
    i am happy

---
+++ index.php
@@ -0,0 +1,222 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bootstrap</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<!-- Latest Compiled And Minified Css -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+
+<!-- Jquery Library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+<!-- Popper Js -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+<!-- Latest Compiled Javascript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js">
+	</script>
+
+<link rel="stylesheet"  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">	
+<meta name="viewport" content="width=device-width, intial-scale-1">
+</head>
+<body>
+<div class="bgimg">
+<nav class="navbar navbar-expand-md bg-dark navbar-dark fixed-top">
+
+<div class="container">
+	<a href="" class="navbar-brand text-warning font-weight-bold"> SHIVAM DIXIT</a>
+
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsenavbar">
+		<span class="navbar-toggler-icon"></span>
+		</button>
+
+		<div class="collapse navbar-collapse text-center" id="collapsenavbar">
+			<ul class="navbar-nav ml-auto">
+				<li class="nav-item">
+					<a href="" class="nav-link text-white">SERVICES</a>
+				</li>
+				<li class="nav-item">
+					<a href="" class="nav-link text-white">PORTFOLIO</a>
+				</li>
+				<li class="nav-item">
+					<a href="" class="nav-link text-white">ABOUT</a>
+				</li>
+				<li class="nav-item">
+					<a href="" class="nav-link text-white">TEAM</a>
+				</li>
+				<li class="nav-item">
+					<a href="" class="nav-link text-white">MOBILE</a>
+				</li>
+				
+			</ul>
+			
+		</div>
+</div>
+</nav>
+
+<div class="container text-center text-dark hearderset">
+	<h2>Welcome to our stdio!!</h2>
+	<h1>IT'S NICE TO MEET YOU</h1>
+	<button class="btn btn-warning text-white btn-lg">SUBSCRIBE</button>
+</div>
+</div>
+
+<section class="container ourservices text-center">
+	<h1>SERVICES</h1>
+	<p> Lorem ipsum dolor sit amet consectetur</p>
+	<div class="row rowsetting">
+		<div class="col-lg-4 col-md-4 col-sm-4 col-10 d-block m-auto">
+			<div class="imgsetting d-block m-auto bg-warning"> 
+				<i class="fa fa-shopping-cart fa-3x text-white"> </i>
+			</div>
+			<h2> E-Commerce</h2>
+			<p>Lorem ipsum dolor sit amet, consecteutr adipisicing elit. Minima maxima
+			quae archicture quo inventure harum ex magni, dicta impedit</p>
+		</div>
+
+
+		<div class="col-lg-4 col-md-4 col-sm-4 col-10 d-block m-auto">
+			<div class="imgsetting d-block m-auto bg-warning text-white">
+				<i class="fa fa-desktop fa-3x"> </i>
+			</div>
+			<h2> Responsive Design</h2>
+			<p>Lorem ipsum dolor sit amet, consecteutr adipisicing elit. Minima maxima
+			quae archicture quo inventure harum ex magni, dicta impedit</p>
+		</div>
+
+		<div class="col-lg-4 col-md-4 col-sm-4 col-10 d-block m-auto">
+			<div class="imgsetting d-block m-auto bg-warning text-white">
+				<i class="fa fa-lock fa-3x"> </i>
+			</div>
+			<h2> Web Security</h2>
+			<p>Lorem ipsum dolor sit amet, consecteutr adipisicing elit. Minima maxima
+			quae archicture quo inventure harum ex magni, dicta impedit</p>
+		</div>
+	</div>
+
+</section>
+
+
+<!-- /////////////////////////////////////Port polio -->
+
+
+
+<section class="portfolio bg-light">
+	<div class="container text-center">
+		<h1>PORTFOLIO</h1>
+		<p> Lorem ipsum dolor dit amet consectetur</p>
+		
+		<div class="row teamsetting">
+			<div class="col-lg-4 col-md-4 col-sm-12 col-10 d-block m-auto">
+				<div class="card">
+					<img src="img/ss12.jpg" class="card-img img-fluid">
+					<div class="card-body">
+						<h2 class="card-title"> Threads</h2>
+						<p class="card-text">Illustration</p>
+						
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-12 col-10 d-block m-auto">
+				<div class="card">
+					<img src="img/ss8.jpg" class="card-img img-fluid">
+					<div class="card-body">
+						<h2 class="card-title"> Explore</h2>
+						<p class="card-text">Graphic Design</p>
+						
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-12 col-10 d-block m-auto">
+				<div class="card">
+					<img src="img/ss10.jpg" class="card-img img-fluid">
+					<div class="card-body">
+						<h2 class="card-title"> Finish</h2>
+						<p class="card-text">Identify</p>
+						
+					</div>
+				</div>
+			</div>
+
+<div class="row">
+			<div class="col-lg-4 col-md-4 col-sm-12 col-10 d-block m-auto">
+				<div class="card">
+					<img src="img/ss11.jpg" class="card-img img-fluid">
+					<div class="card-body">
+						<h2 class="card-title"> Lines</h2>
+						<p class="card-text">Branding</p>
+						
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-12 col-10 d-block m-auto">
+				<div class="card">
+					<img src="img/ss12.jpg" class="card-img img-fluid">
+					<div class="card-body">
+						<h2 class="card-title"> Southwest</h2>
+						<p class="card-text">Website Design</p>
+						
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-12 col-10 d-block m-auto">
+				<div class="card">
+					<img src="img/ss9.jpg" class="card-img img-fluid">
+					<div class="card-body">
+						<h2 class="card-title"> Window</h2>
+						<p class="card-text"> Photography</p>
+						
+					</div>
+				</div>
+			</div>
+
+
+		</div>
+	</div>
+	</section>
+
+	<section class="ourteam">
+		<div class="container text-center">
+			<h1> OUR AMAZING TEAM</h1>
+			<p>Lore ipsum dolor sit amet consectetur.</p>
+
+
+			<div class="row teamsetting">
+				<div class="col-lg-4 col-md-4 col-sm-10 col-12 d-block m-auto">
+					<figure class="figure">
+						<img src="img/ss12.jpg" class="figure-img img-fluid rounded-circle" height="250px" width="250px">
+						<figcaption>
+							<h4> Jashan Singh </h4>
+							<p class="figure-caption"> Lead Designer</p>
+						</figcaption>
+					</figure>
+					</div>
+					<div class="col-lg-4 col-md-4 col-sm-10 col-12 d-block m-auto">
+					<figure class="figure">
+						<img src="img/skd.jpg" class="figure-img img-fluid rounded-circle" height="250px" width="250px">
+						<figcaption>
+							<h4> Shivam Dixit </h4>
+							<p class="figure-caption"> Web Designer</p>
+						</figcaption>
+					</figure>
+					</div>
+					<div class="col-lg-4 col-md-4 col-sm-10 col-12 d-block m-auto">
+					<figure class="figure">
+						<img src="img/ss8.jpg" class="figure-img img-fluid rounded-circle" height="250px" width="250px">
+						<figcaption>
+							<h4> Ranmandeep Kaur </h4>
+							<p class="figure-caption"> App Devloper</p>
+						</figcaption>
+					</figure>
+					</div>
+				</div>
+			</div>
+
+	</section>
+
+<footer class="footer">
+	<h6 class="text-center">copyright @ 2020, Design by shivam kumar dixit 
+</footer>
+
+</body>
+</html>
